1fa02a33a1040a9649f42e0ab557b6c7dbd5faa3
Ian Paulus Sinambela <Ian.Sinambela@ag-it.com>
2024-10-21 15:33:38 +0000

    Updated app.js

--- poc/app.js
+++ poc/app.js
@@ -6,7 +6,7 @@ const port = 3001;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Imanuel Sianturi\n');
+  res.end('Hello, Imanuel Sianturii\n');
 });
 
 server.listen(port, hostname, () => {
