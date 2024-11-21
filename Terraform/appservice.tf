1164eba71317ba8b46365de02df466aea00a0afa
immanuel12 <50264708+immanuel12@users.noreply.github.com>
2024-11-21 16:07:39 +0700

    update

--- Terraform/appservice.tf
+++ Terraform/appservice.tf
@@ -21,13 +21,13 @@ resource "azurerm_app_service" "app_service" {
     always_on = true
     default_documents        = ["Default.htm","Default.html","Default.asp","index.htm","index.html","iisstart.htm","default.aspx","index.php","hostingstart.html",]
     dotnet_framework_version = "v6.0"
-    scm_type                 = "None"
+    scm_type                 = "VSTSRM"
     use_32_bit_worker_process = true
   }
 
-#   connection_string {
-#     name  = "Database"
-#     type  = "SQLServer"
-#     value = "Server=sql-devsecops.database.windows.net;Database=db-devsecops;User ID=sqladmin;Password=P@ssw0rd123!;Encrypt=true;TrustServerCertificate=false;Connection Timeout=30;"
-#   }
+  connection_string {
+    name  = "sql-devsecops"
+    type  = "SQLServer"
+    value = "Server=tcp:sql-devsecops.database.windows.net,1433;Initial Catalog=db-devsecops;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
 }
