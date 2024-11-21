5fb7a329be2a6fe7d70777f35eddac4aa9765361
immanuel12 <50264708+immanuel12@users.noreply.github.com>
2024-11-21 12:51:19 +0700

    update

--- Terraform/appservice.tf
+++ Terraform/appservice.tf
@@ -1,32 +1,32 @@
-# resource "azurerm_app_service_plan" "example" {
-#   name                = "appserviceplan-${var.project_name}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "appserviceplan-${var.project_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
-#   sku {
-#     tier = "Standard"
-#     size = "S1"
-#   }
-# }
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
 
-# resource "azurerm_app_service" "example" {
-#   name                = "app-service-${var.project_name}"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   app_service_plan_id = azurerm_app_service_plan.example.id
+resource "azurerm_app_service" "app_service" {
+  name                = "app-service-${var.project_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
 
-#   site_config {
-#     dotnet_framework_version = "v4.0"
-#     scm_type                 = "LocalGit"
-#   }
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
 
-#   app_settings = {
-#     "SOME_KEY" = "some-value"
-#   }
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
 
-#   connection_string {
-#     name  = "Database"
-#     type  = "SQLServer"
-#     value = "Server=sql-devsecops.database.windows.net;Database=db-devsecops;User ID=sqladmin;Password=P@ssw0rd123!;Encrypt=true;TrustServerCertificate=false;Connection Timeout=30;"
-#   }
-# }
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=sql-devsecops.database.windows.net;Database=db-devsecops;User ID=sqladmin;Password=P@ssw0rd123!;Encrypt=true;TrustServerCertificate=false;Connection Timeout=30;"
+  }
+}
--- Terraform/azure_sql.tf
+++ Terraform/azure_sql.tf
@@ -1,56 +1,56 @@
-# resource "azurerm_mssql_server" "server_dev" {
-#   name                         = "sql-${var.project_name}"
-#   resource_group_name          = azurerm_resource_group.rg.name
-#   location                     = azurerm_resource_group.rg.location
-#   version                      = "12.0"
-#   administrator_login          = "sqladmin"
-#   administrator_login_password = "P@ssw0rd123!"
+resource "azurerm_mssql_server" "server_dev" {
+  name                         = "sql-${var.project_name}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladmin"
+  administrator_login_password = "P@ssw0rd123!"
 
-#   # tags = merge(
-#   #   {"Environment" = "${var.environments}"},
-#   #   {"BillingItem" = "Azure SQL Server"}
-#   # )
+  # tags = merge(
+  #   {"Environment" = "${var.environments}"},
+  #   {"BillingItem" = "Azure SQL Server"}
+  # )
+}
+
+resource "azurerm_mssql_database" "db_dev" {
+  name           = "db-${var.project_name}"
+  server_id      = azurerm_mssql_server.server_dev.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  max_size_gb    = 256
+  read_scale     = false
+  min_capacity   = 1
+  sku_name       = "GP_S_Gen5_2"
+  zone_redundant = false
+  storage_account_type = "Local"
+  auto_pause_delay_in_minutes = -1
+
+  # tags = merge(
+  #   {"Environment" = "${var.environments}"},
+  #   {"BillingItem" = "Azure SQL Database"}
+  # )
+
+  # #prevent the possibility of accidental data loss
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+}
+
+# resource "azurerm_private_endpoint" "pe_sql_dev" {
+
+#   name                = "pe-sql-${var.project_name}-${var.environment}"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   subnet_id           = module.virtual_network.subnet_ids[4]
+
+#   private_service_connection {
+#     name                           = "ip-pe-sql-${var.project_name}-${var.environment}"
+#     is_manual_connection           = false
+#     private_connection_resource_id = azurerm_mssql_server.server_dev.id
+#     subresource_names              = ["sqlServer"]  
+#   }
+
+#   tags = merge(
+#     {"Environment" = "${var.environments}"},
+#     {"BillingItem" = "Azure SQL Database"}
+#   )  
 # }
-
-# resource "azurerm_mssql_database" "db_dev" {
-#   name           = "db-${var.project_name}"
-#   server_id      = azurerm_mssql_server.server_dev.id
-#   collation      = "SQL_Latin1_General_CP1_CI_AS"
-#   max_size_gb    = 256
-#   read_scale     = false
-#   min_capacity   = 1
-#   sku_name       = "GP_S_Gen5_2"
-#   zone_redundant = false
-#   storage_account_type = "Local"
-#   auto_pause_delay_in_minutes = -1
-
-#   # tags = merge(
-#   #   {"Environment" = "${var.environments}"},
-#   #   {"BillingItem" = "Azure SQL Database"}
-#   # )
-
-#   # #prevent the possibility of accidental data loss
-#   # lifecycle {
-#   #   prevent_destroy = true
-#   # }
-# }
-
-# # resource "azurerm_private_endpoint" "pe_sql_dev" {
-
-# #   name                = "pe-sql-${var.project_name}-${var.environment}"
-# #   location            = azurerm_resource_group.rg.location
-# #   resource_group_name = azurerm_resource_group.rg.name
-# #   subnet_id           = module.virtual_network.subnet_ids[4]
-
-# #   private_service_connection {
-# #     name                           = "ip-pe-sql-${var.project_name}-${var.environment}"
-# #     is_manual_connection           = false
-# #     private_connection_resource_id = azurerm_mssql_server.server_dev.id
-# #     subresource_names              = ["sqlServer"]  
-# #   }
-
-# #   tags = merge(
-# #     {"Environment" = "${var.environments}"},
-# #     {"BillingItem" = "Azure SQL Database"}
-# #   )  
-# # }
