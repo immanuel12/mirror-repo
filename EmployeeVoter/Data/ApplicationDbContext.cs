15ec5c77ec057dfa39a216b00cc5edf9db22138f
immanuel12 <50264708+immanuel12@users.noreply.github.com>
2024-11-28 09:40:57 +0700

    update

---
+++ EmployeeVoter/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeVoter.Models.Entitites;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeVoter.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<Candidate> Candidates { get; set; }
+        public virtual DbSet<ChoiceVoter> ChoiceVoter { get; set; }
+    }
+}
